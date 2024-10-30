4d0e5f15a6ec102fe93a4958870ebe8368eb0abd
Farhan Atif <farhanatif9990@gmail.com>
2024-10-30 18:52:24 +0500

    g.o.od

---
+++ Models/Transaction.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_App.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //Foriegn Key
+        [Range(1,int.MaxValue,ErrorMessage ="Please Select Category")]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+        
+        [Range(1,int.MaxValue,ErrorMessage ="Amount Should be greater than 0")]
+         public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string? Note { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string CategoryTitleWithIcon { get {
+                return Category == null ? "" :Category.TitleWithIcon;
+            } 
+        }
+        
+        [NotMapped]
+        public string? FormatedAmount { get {
+                return ((Category == null || Category.Type == "Expense")  ? "- " : "+ ")+Amount.ToString("C0");
+            } 
+        }
+
+    }
+}
